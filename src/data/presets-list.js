e1d67d55d760fe93294a964bb1ffc3b7ddc53709
Brian Manley <brian.manley91@gmail.com>
2022-11-08 18:35:13 -0500

    Better fit in dropdown

--- src/data/presets-list.js
+++ src/data/presets-list.js
@@ -1,24 +1,24 @@
 export const presets = {
     nycMenHalfQualifier: {
-        displayName: 'NYC Marathon Qualifier Half-Marathon (Men 18-34)',
+        displayName: 'NYC Half-Marathon Qualifier (M 18-34)',
         distance: 13.1,
         time: '1:21:00',
         pace: '6:11',
     },
     nycMenFullQualifier: {
-        displayName: 'NYC Marathon Qualifier Marathon (Men 18-34)',
+        displayName: 'NYC Marathon Qualifier (M 18-34)',
         distance: 26.2,
         time: '2:53:00',
         pace: '6:37',
     },
     nycWomenHalfQualifier: {
-        displayName: 'NYC Marathon Qualifier Half-Marathon (Women 18-34)',
+        displayName: 'NYC Half-Marathon Qualifier (F 18-34)',
         distance: 13.1,
         time: '1:32:00',
         pace: '7:02',
     },
     nycWomenFullQualifier: {
-        displayName: 'NYC Marathon Qualifier Marathon (Women 18-34)',
+        displayName: 'NYC Marathon Qualifier (F 18-34)',
         distance: 26.2,
         time: '3:13:00',
         pace: '7:22',
