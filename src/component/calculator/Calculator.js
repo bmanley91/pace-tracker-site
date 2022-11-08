27ffbc414134b200326b199f4715621210a4cc53
Brian Manley <brian.manley91@gmail.com>
2022-11-07 22:21:32 -0500

    Style and layout cleanups (#9)
    * Added favicon
    
    * Updated title
    
    * Handle mobile view
    
    * Remove extra minwidth
    
    * Adjusted help popover size

--- src/component/calculator/Calculator.js
+++ src/component/calculator/Calculator.js
@@ -1,5 +1,5 @@
+import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
-import Box from '@mui/system/Box';
 import React, { useEffect, useState } from 'react';
 import { calculatePace, calulateTime } from '../../util/calculators';
 import {
@@ -45,55 +45,62 @@ const Calculator = () => {
     }, [distance, pace, time]);
 
     return (
-        <Box
-            component="form"
+        <Grid
+            container
             alignItems="center"
-            justifyContent="center"
-            display="flex"
+            justify="center"
+            style={{ textAlign: 'center' }}
         >
-            <TextField
-                sx={{ mx: 2 }}
-                label="Distance (mi)"
-                value={distance}
-                onChange={(event) => {
-                    setDistance(event.target.value);
-                }}
-                onFocus={() => setFocus('distance')}
-                helperText="Distance in miles"
-            />
-            <TextField
-                sx={{ mx: 2 }}
-                label="Pace (min/mi)"
-                value={pace}
-                onChange={(event) => {
-                    if (paceRegex.test(event.target.value)) {
-                        setIsPaceValid(true);
-                    } else {
-                        setIsPaceValid(false);
-                    }
-                    setPace(event.target.value);
-                }}
-                onFocus={() => setFocus('pace')}
-                error={!isPaceValid}
-                helperText="Format mm:ss"
-            />
-            <TextField
-                sx={{ mx: 2 }}
-                label="Time"
-                value={time}
-                onChange={(event) => {
-                    if (timeRegex.test(event.target.value)) {
-                        setIsTimeValid(true);
-                    } else {
-                        setIsTimeValid(false);
-                    }
-                    setTime(event.target.value);
-                }}
-                error={!isTimeValid}
-                onFocus={() => setFocus('time')}
-                helperText="Format mm:ss or hh:mm:ss"
-            />
-        </Box>
+            <Grid item xs={12} md={4}>
+                <TextField
+                    sx={{ mx: 2 }}
+                    label="Distance (mi)"
+                    value={distance}
+                    onChange={(event) => {
+                        setDistance(event.target.value);
+                    }}
+                    onFocus={() => setFocus('distance')}
+                    helperText="Distance in miles"
+                />
+            </Grid>
+            <Grid item xs={12} md={4}>
+                <TextField
+                    sx={{ mx: 2 }}
+                    label="Pace (min/mi)"
+                    value={pace}
+                    onChange={(event) => {
+                        if (paceRegex.test(event.target.value)) {
+                            setIsPaceValid(true);
+                        } else {
+                            setIsPaceValid(false);
+                        }
+                        setPace(event.target.value);
+                    }}
+                    onFocus={() => setFocus('pace')}
+                    error={!isPaceValid}
+                    helperText="Format mm:ss"
+                />
+            </Grid>
+
+            <Grid item xs={12} md={4}>
+                <TextField
+                    sx={{ mx: 2 }}
+                    label="Time"
+                    value={time}
+                    onChange={(event) => {
+                        if (timeRegex.test(event.target.value)) {
+                            setIsTimeValid(true);
+                        } else {
+                            setIsTimeValid(false);
+                        }
+                        setTime(event.target.value);
+                    }}
+                    error={!isTimeValid}
+                    onFocus={() => setFocus('time')}
+                    helperText="Format mm:ss or hh:mm:ss"
+                />
+            </Grid>
+        </Grid>
     );
 };
 
