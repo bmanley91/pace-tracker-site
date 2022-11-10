05ef923b12329ad88e82651dd843e307428b2e1a
Brian Manley <brian.manley91@gmail.com>
2022-11-09 21:14:30 -0500

    Added basic analytics

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import Container from '@mui/material/Container';
 import Header from './component/header/Header';
 import BodyContainer from './component/body-container/BodyContainer';
 import Footer from './component/footer/Footer';
+import { logAnalyticsEvent } from './util/firebase';
 
 const App = () => {
+    logAnalyticsEvent('page-load');
+
     return (
         <Container>
             <Header />
